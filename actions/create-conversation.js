eb4113b515d807e635816871a6119835341d9f73
ivanpedrocr <casasivan530@gmail.com>
2021-12-01 13:20:57 -0500

    don't create conversation if user already exists

--- actions/create-conversation.js
+++ actions/create-conversation.js
@@ -2,6 +2,23 @@ import firebase from "firebase";
 
 const createConversation = async (user1, { userId }) => {
   const db = firebase.database();
+  const userConversations = await (
+    await db.ref(`users/${userId}/conversations`).get()
+  ).val();
+  const conversationUsers = await Promise.all(
+    Object.values(userConversations).map(async (conversation) => {
+      const users = await (
+        await db.ref(`conversations/${conversation}/users`).get()
+      ).val();
+      return { users: Object.values(users), conversation };
+    })
+  );
+  const currentConversation = conversationUsers.filter((conversation) =>
+    conversation.users.includes(user1)
+  );
+  if (currentConversation[0].conversation) {
+    return currentConversation[0].conversation;
+  }
   try {
     if (user1 && userId) {
       const pushKey = db.ref("conversations").push().key;
@@ -9,12 +26,11 @@ const createConversation = async (user1, { userId }) => {
       await db.ref(`/users/${userId}/conversations`).push(pushKey);
       await db.ref(`/conversations/${pushKey}/users`).push(user1);
       await db.ref(`/conversations/${pushKey}/users`).push(userId);
+      return pushKey;
     }
   } catch (e) {
     console.log(e);
   }
-
-  return pushKey;
 };
 
 export default createConversation;
--- components/BasicForm.js
+++ components/BasicForm.js
@@ -1,5 +1,5 @@
 import RNDateTimePicker from "@react-native-community/datetimepicker";
-import React, { useState } from "react";
+import React from "react";
 import { View } from "react-native";
 import { useColor } from "../hooks/useColor";
 import { AppTextInput } from "./layout/Native-components";
